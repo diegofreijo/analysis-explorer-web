9e6c13b58e1290327c5863ac7e01208cec1cca2e
Diego Freijo <giga.freijo@gmail.com>
2018-08-28 07:46:31 -0300

    - Main.elm wasn't merged from the dagre branch, don't know why

--- Main.elm
+++ Main.elm
@@ -1,8 +1,11 @@
-module Main exposing (..)
+-- Test with
+--      elm-live Main.elm --output dist/main.js
+
+
+port module Main exposing (..)
 
 import Html exposing (div, button, text)
-import JsonDecoder exposing (decode, JsonESG, JsonEG, JsonNode, JsonEdge)
-import Draw exposing (..)
+import EsgDecoder exposing (decode, JsonESG, JsonEG, JsonNode, JsonEdge)
 
 
 -- import Html.Events exposing (onClick)
@@ -24,7 +27,79 @@ main =
 
 
 type alias Model =
-    {}
+    { nodes : List Node
+    , edges : List Edge
+    }
+
+
+type alias Node =
+    { data : NodeData
+    , classes : Class
+    , position : NodePosition
+    }
+
+
+type alias NodeData =
+    { id : NodeId
+    , name : String
+    , parent : Maybe NodeId
+    }
+
+
+type alias Class =
+    String
+
+
+type alias NodeId =
+    String
+
+
+type alias NodePosition =
+    { x : Int
+    , y : Int
+    }
+
+
+type alias EdgeId =
+    String
+
+
+type alias Edge =
+    { data :
+        { id : EdgeId
+        , source : NodeId
+        , target : NodeId
+        }
+    , classes : Class
+    }
+
+
+
+-- CONSTANTS
+
+
+emptyModel : Model
+emptyModel =
+    { nodes = [], edges = [] }
+
+
+instructionClass : String
+instructionClass =
+    "instruction"
+
+
+methodClass : String
+methodClass =
+    "method"
+
+
+interproceduralEdgeClass : String
+interproceduralEdgeClass =
+    "interproceduralEdge"
+
+intraproceduralEdgeClass : String
+intraproceduralEdgeClass =
+    "intraproceduralEdge"
 
 
 
@@ -34,118 +109,96 @@ type alias Model =
 init : ( Model, Cmd Msg )
 init =
     let
-        graph =
+        model =
             case decode of
                 Ok jsonEsg ->
                     convertEsg jsonEsg
 
                 Err error ->
                     -- TODO: Improve this error handling
-                    Draw.graph [] []
+                    emptyModel
     in
-        ( {}, Draw.drawGraph graph )
+        ( model, drawCytoscape model )
 
 
-convertEsg : JsonESG -> Draw.Graph
+convertEsg : JsonESG -> Model
 convertEsg { methods } =
+    List.foldr
+        (\m acc ->
+            Model
+                (List.append m.nodes acc.nodes)
+                (List.append m.edges acc.edges)
+        )
+        emptyModel
+        (List.indexedMap convertEg methods)
+
+
+convertEg : Int -> JsonEG -> Model
+convertEg column { name, nodes, edges } =
     let
-        nodes =
-            (Draw.instructionNode "1" "I1" Nothing 0 0)
-                :: (Draw.instructionNode "2" "I2" Nothing 0 100)
-                :: []
+        x =
+            column * 250
 
-        edges =
-            []
+        methodNode =
+            createMethodNode name name x
+
+        newNodes =
+            methodNode
+                :: List.indexedMap (convertInstructionNode name x) nodes
     in
-        Draw.graph nodes edges
+        { nodes = newNodes
+        , edges = List.map (convertEdge newNodes) edges
+        }
+
+
+createMethodNode : String -> NodeId -> Int -> Node
+createMethodNode name id x =
+    { data =
+        { id = id
+        , name = "Method:\n" ++ name
+        , parent = Nothing
+        }
+    , classes = methodClass
+    , position = { x = x, y = 0 }
+    }
+
+
+convertInstructionNode : NodeId -> Int -> Int -> JsonNode -> Node
+convertInstructionNode parent x row { id, kind } =
+    { data =
+        { id = toString id
+        , name = "Instruction:\n" ++ toString id
+        , parent = Just parent
+        }
+    , classes = instructionClass
+    , position = { x = x, y = row * 100 }
+    }
+
+
+convertEdge : List Node -> JsonEdge -> Edge
+convertEdge nodes { origin, destination, kind } =
+    let
+        source =
+            (toString origin)
+
+        target =
+            (toString destination)
+    in
+        { data =
+            { id = toString (source ++ target)
+            , source = source
+            , target = target
+            }
+        , classes = 
+            case kind of
+                0 -> interproceduralEdgeClass
+                _ -> intraproceduralEdgeClass
+        }
 
 
 
--- List.foldr
---     (\m acc ->
---         Model
---             (List.append m.nodes acc.nodes)
---             (List.append m.edges acc.edges)
---     )
---     emptyModel
---     (List.indexedMap convertEg methods)
-
-
--- convertEg : Int -> JsonEG -> Model
--- convertEg column { name, nodes, edges } =
---     let
---         x =
---             column * 250
-
---         methodNode =
---             createMethodNode name name x
-
---         newNodes =
---             methodNode
---                 :: List.indexedMap (convertInstructionNode name x) nodes
---     in
---         { nodes = newNodes
---         , edges = List.map (convertEdge newNodes) edges
---         }
-
-
--- createMethodNode : String -> NodeId -> Int -> Node
--- createMethodNode name id x =
---     { data =
---         { id = id
---         , name = "Method:\n" ++ name
---         , parent = Nothing
---         }
---     , classes = methodClass
---     , position = { x = x, y = 0 }
---     }
-
-
-
--- convertInstructionNode : NodeId -> Int -> Int -> JsonNode -> Node
--- convertInstructionNode parent x row { id, kind } =
---     { data =
---         { id = toString id
---         , name = "Instruction:\n" ++ toString id
---         , parent = Just parent
---         }
---     , classes = "node " ++
---         case kind of
---             0 ->
---                 "entryNode"
---             1 ->
---                 "exitNode"
---             _ ->
---                 instructionClass
---     , position = { x = x, y = row * 100 }
---     }
-
-
--- convertEdge : List Node -> JsonEdge -> Edge
--- convertEdge nodes { origin, destination, kind } =
---     let
---         source =
---             (toString origin)
-
---         target =
---             (toString destination)
---     in
---         { data =
---             { id = toString (source ++ target)
---             , source = source
---             , target = target
---             }
---         , classes =
---             case kind of
---                 0 ->
---                     interproceduralEdgeClass
-
---                 _ ->
---                     intraproceduralEdgeClass
---         }
-
-
-
+-- HELPERS
+-- wnodesWithParents : List Node ->  -> ()
 -- UPDATE
 
 
@@ -165,16 +218,18 @@ update msg model =
 
 view : Model -> Html.Html Msg
 view model =
-    text ":)"
-    -- model.nodes
-    --     |> List.length
-    --     |> toString
-    --     |> (++) "#Nodes: "
-    --     |> text
+    model.nodes
+        |> List.length
+        |> toString
+        |> (++) "#Nodes: "
+        |> text
 
 
 
 -- PORTS
+
+
+port drawCytoscape : Model -> Cmd msg
 
 
 subscriptions : Model -> Sub Msg
